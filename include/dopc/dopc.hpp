c5232050c6ec6f231bc30aaceef90048eb41331f
Matthew Smith <matthew.peter.smith.599@gmail.com>
2022-12-10 02:08:37 +0000

    Fixed misplaced `#pragma once`.

--- include/dopc/dopc.hpp
+++ include/dopc/dopc.hpp
@@ -5,7 +5,9 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at 
  * https://mozilla.org/MPL/2.0/.
 
- */ #pragma once
+ */ 
+
+#pragma once
 
 #include<iostream>
 #include<vector> 
